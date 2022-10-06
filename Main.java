1c696eb4022b18cecbc593db71cec93fe4c62b41
LuuLeThaiSon <llts28497@gmail.com>
2022-10-06 08:59:28 +0700

    done

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1, num2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number 1: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("Enter number 2: ");
+        num2 = scanner.nextInt();
+
+        num1 = Math.abs(num1);
+        num2 = Math.abs(num2);
+
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("No greatest common factor");
+        } else {
+            while (num1 != num2) {
+                if (num1 > num2)
+                    num1 = num1 - num2;
+                else
+                    num2 = num2 - num1;
+            }
+            System.out.println("Greatest common factor: " + num1);
+        }
+    }
+}
